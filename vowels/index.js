e0ce1744e02b01c2b28eba73693cfe2ea4519af0
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-31 18:04:13 +0100

    Add vowels test

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+
+    // let vowels = 0;
+    //
+    // const vowelSelect = "aeiou";
+    //
+    // for(let char of str.toLowerCase()) {
+    //     if(vowelSelect.includes(char)) {
+    //         vowels ++
+    //     }
+    // }
+    // return vowels;
+    const vowelArray = str.match(/[aeiou]/gi);
+
+    return vowelArray ? vowelArray.length : 0;
+}
 
 module.exports = vowels;
+
