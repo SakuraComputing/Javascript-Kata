3975b977fd85762889443717348679ea74db56a2
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-31 17:00:00 +0100

    Create Random test generator

--- randomTest/index.js
+++ randomTest/index.js
@@ -7,17 +7,25 @@ const randomTestSelector = () => {
 
     let exercises = [];
 
+    // Access file system to return directories
     filesystem.readdirSync(dir).forEach((file =>  {
 
         const stat = filesystem.statSync(file);
 
-        if (stat.isDirectory() && file !== 'randomTestSelector' && file !=='node_modules') {
+        if (stat.isDirectory() && file !== 'randomTestSelector'
+            && file !=='node_modules'
+            && file !== '.git') {
             exercises.push(file)
         }
 
     }));
 
-    return exercises;
+    // Choose at random an exercise
+    const random = Math.floor(Math.random() * exercises.length);
+    const chosenExercise = exercises[random];
+    console.log(chosenExercise);
+
+    return chosenExercise;
 
 };
 module.exports = randomTestSelector;
--- randomTest/test.js
+++ randomTest/test.js
@@ -2,13 +2,56 @@ const path = require('path');
 const randomTestSelector = require('./index');
 
 describe('randomTest suite', () => {
+
+    beforeEach(() => {
+        jest.spyOn(console, 'log');
+    });
+
+    afterEach(() => {
+        console.log.mockRestore();
+    });
+
    it('should be a function', () => {
        expect(typeof randomTestSelector).toEqual('function');
    });
-   it('should produce an array with 28 elements in it', () => {
-        expect(randomTestSelector()).toHaveLength(28);
-   });
-   // it('should return one test at random', () => {
-   //
+   // it('should produce an array with 28 exercises in it', () => {
+   //      const exercises = randomTestSelector();
+   //      expect(randomTestSelector()).toHaveLength(28);
    // });
+   it('should return one exercise', () => {
+       const exercises = [ 'anagrams',
+           'bst',
+           'capitalize',
+           'chunk',
+           'circular',
+           'events',
+           'fib',
+           'fizzbuzz',
+           'fromlast',
+           'levelwidth',
+           'linkedlist',
+           'matrix',
+           'maxchar',
+           'midpoint',
+           'palindrome',
+           'pyramid',
+           'qfroms',
+           'queue',
+           'randomTest',
+           'reverseint',
+           'reversestring',
+           'sorting',
+           'stack',
+           'steps',
+           'tree',
+           'validate',
+           'vowels',
+           'weave' ];
+
+       expect(exercises).toContain(randomTestSelector());
+   });
+   it('should output the exercise via the console', () => {
+       randomTestSelector();
+       expect(console.log).toHaveBeenCalled();
+   })
 });
